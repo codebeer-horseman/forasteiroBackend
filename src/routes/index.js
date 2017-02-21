ec86ef963f3c73e7b342694373d39eef8635e9c4
Daniel Barreto <dandpb@gmail.com>
2017-02-21 01:58:50 -0300

    fixed url routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,10 @@ import apiUser from '../controllers/apiUsers';
 
 let router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/api/v1', (req, res) => {
     res.render('index', {title: 'Pug - Fun', currentTime: req.requestTime});
 });
 
-router.use('/users', apiUser.getAll);
+router.use('/api/users/v1', apiUser.getAll);
 
 export default router;
